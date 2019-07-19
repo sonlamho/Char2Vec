638e63b2d579dfe545f106b2fb9d49ba24e6293f
Lam Son Ho <sonlam@math.umd.edu>
2019-07-19 16:46:46 +0700

    defined graph for Char2Vec

--- charem/embed.py
+++ charem/embed.py
@@ -1,25 +1,67 @@
 
 import numpy as np
 import tensorflow as tf
-from utils import Tokenizer, ALPHABET
+from .utils import Tokenizer, ALPHABET, normalized
 
 class CONFIG:
-  # embedding dimension
-  N = 20
+  """Model hyperparams"""
+  D = 20          # embedding dimension
 
   # left and right window size
   L_WINDOW = 2
   R_WINDOW = 2
 
+  BATCH = 128
+  EPOCHS = 30
 
-class Word2Char(object):
 
-  def __init__(self):
-    pass
+class Char2Vec(object):
+
+  N_HEADS = 3
+
+  def __init__(self, config=CONFIG, alphabet=ALPHABET, unk='~', DTYPE='float32'):
+    self.cfg = config
+    self.graph = tf.Graph()
+    self.DTYPE = DTYPE
+    self.tokenizer = Tokenizer(alphabet, unk)
+    self.V = self.tokenizer.V
+
+  def create_graph(self):
+    with self.graph.as_default():
+      self.x_in = tf.placeholder(dtype=self.DTYPE, shape=[None, self.V], name='x_in')  #  batch_size will replace None in shape
+      self.U = tf.get_variable(dtype=self.DTYPE, shape=[self.V, self.cfg.D], name='U')
+      self.rep = tf.matmul(self.x_in, self.U)   # shape [batch_size, D]
+      self.Ws, self.logits = self._construct_logits(self.rep, self.N_HEADS)
+      #self.y_hats = [tf.nn.softmax(t, axis=-1) for t in self.logits]
+      self.y_labels = [
+        tf.placeholder(dtype=self.DTYPE, shape=[None, self.V])
+        for i in range(self.N_HEADS)
+      ]
+      self.losses = [
+        tf.nn.softmax_cross_entropy_with_logits_v2(
+          labels=label, logits=logit)
+        for logit, label in zip(self.logits, self.y_labels)
+      ]
+      self.loss = tf.add_n([tf.reduce_mean(t) for t in self.losses])
+      self._optimizer = tf.train.AdamOptimizer()
+      self.train_step = self._optimizer.minimize(self.loss)
+
+  def _construct_logits(self, rep, n_heads):
+    """ """
+    Ws = []
+    logits = []
+    with self.graph.as_default():
+      for i in range(n_heads):
+        weights_i = tf.get_variable(dtype=self.DTYPE, shape=[self.cfg.D, self.V], name='W_'+str(i))
+        logits_i = tf.matmul(rep, weights_i)
+        Ws.append(weights_i)
+        logits.append(logits_i)
+    return Ws, logits
 
   def fit(self, path_to_corpus):
-    pass
+    pass #TODO
 
 
 class CharGloVe(object):
+  # TODO
   pass
--- charem/utils.py
+++ charem/utils.py
@@ -6,12 +6,15 @@ ALPHABET = """abcdefghijklmnopqrstuvwxyz1234567890,.()[]"' -\n"""
 
 class Tokenizer(object):
 
-  def __init__(self, alphabet=ALPHABET):
+  def __init__(self, alphabet=ALPHABET, unk='~'):
+    assert unk not in alphabet, "please keep UNK character not part of alphabet"
     self.alphabet = sorted(list(set(alphabet)))
     self._to_int = {  # dict mapping known characters to integers
       c:i for i,c in enumerate(self.alphabet)
     }
     self._unk_i = len(self.alphabet)  # int for unknown chars
+    self.unk = unk
+    self.V = len(self.alphabet) + 1   # dim of 1-hot array
 
   def to_ints(self, text):
     """Returns a list of integers"""
@@ -21,11 +24,20 @@ class Tokenizer(object):
     """Return an array of shape `(len(text), len(self.alphabet)+1)`."""
     tokens = self.to_ints(text)
     arr = np.zeros(shape=(len(text), len(self.alphabet)+1), dtype='float32')
-    for pos, tok in enumerate(tokens):
-      arr[pos, tok] = 1
+    # each row of arr represent a character of text
+    for i_row, tok in enumerate(tokens):
+      arr[i_row, tok] = 1
     return arr
 
   def from_ints(self, L):
     """Reconstruct the text given list of integer tokens"""
-    # TODO:
-    pass
+    return ''.join([
+      self.alphabet[i] if i < len(self.alphabet) else self.unk for i in L ])
+
+  def from_1hot(self, arr):
+    """Reconstruct the text given an array"""
+    return self.from_ints([row.argmax() for row in arr])
+
+def normalized(arr):
+  total = arr.sum()
+  return arr / total
